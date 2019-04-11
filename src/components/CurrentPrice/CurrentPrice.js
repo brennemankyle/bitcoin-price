60b31469cafd1682ca6a13179c37eda82cc9af65
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-04-11 17:31:45 -0600

    Make price look a little nicer

--- src/components/CurrentPrice/CurrentPrice.js
+++ src/components/CurrentPrice/CurrentPrice.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
-import { Box, Heading } from 'grommet'
+import { Box, Heading, Grid } from 'grommet'
 import _map from 'lodash/map'
+import coins from '../../util/coins/coins'
 
 class CurrentPrice extends Component {
   render() {
     return (
       <Box>
         <Heading>Current Price</Heading>
+        {this.renderPrice('Name', 'Abbv', 'Rate (BTC)', 'brand')}
         {this.renderPrices()}
       </Box>
     )
   }
 
   renderPrices = () => {
-    return _map(this.props.prices, (value, key) => <div>{key} {value}</div>)
+    return _map(this.props.prices, (value, key) => this.renderPrice(key, coins[key], value))
+  }
+
+  renderPrice = (name, abbv, rate, background = '') => {
+    return (
+      <Grid
+        key={abbv}
+        columns={{
+          count: 3,
+          size: "auto"
+        }}
+        gap="small"
+      >
+        <Box background={background}>{name}</Box>
+        <Box background={background}>{abbv}</Box>
+        <Box background={background}>{rate}</Box>
+      </Grid>
+    )
   }
 }
 
--- src/containers/CurrentPriceContainer/CurrentPriceContainer.js
+++ src/containers/CurrentPriceContainer/CurrentPriceContainer.js
@@ -5,14 +5,10 @@ import coins from '../../util/coins/coins'
 import _map from 'lodash/map'
 import _clone from 'lodash/clone'
 
-const OUTPUT_VALUE = coins.Bitcoin
-
-const displayCoins = _clone(coins)
-delete displayCoins.Bitcoin
-
 class CurrentPriceContainer extends Component {
   static defaultProps = {
-    timerMillisecond: 3000
+    timerMillisecond: 3000,
+    outputValue: 'Bitcoin',
   }
 
   constructor(props) {
@@ -21,6 +17,9 @@ class CurrentPriceContainer extends Component {
     this.state = {
       prices: {}
     }
+
+    this.displayCoins = _clone(coins)
+    delete this.displayCoins[this.props.outputValue]
   }
 
   render() {
@@ -32,12 +31,12 @@ class CurrentPriceContainer extends Component {
   }
 
   getPrices = () => {
-    let promises = _map(displayCoins, (value) => new Promise((resolve, reject) => resolve({data: {rate: Math.random()}}))) // Test Data
-    // let promises = _map(displayCoins,
+    let promises = _map(this.displayCoins, (value) => new Promise((resolve, reject) => resolve({data: {rate: Math.random()}}))) // Test Data
+    // let promises = _map(this.displayCoins,
     //   (value) => coinApi.get(`/exchangerate/${value}/${OUTPUT_VALUE}`))
 
     Promise.all(promises).then((values) => {
-      let prices = Object.keys(displayCoins).reduce((acc, key, index) => {
+      let prices = Object.keys(this.displayCoins).reduce((acc, key, index) => {
         acc[key] = values[index].data.rate
 
         return acc
