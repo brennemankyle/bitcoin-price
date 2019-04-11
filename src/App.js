4b6a8cb62e2f4e7a3eb201a5f57815bbc4f2a7e7
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-04-11 14:20:46 -0600

    Add current price component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './App.scss'
-import PriceList from './components/PriceList/PriceList'
+import CurrentPrice from './components/CurrentPrice/CurrentPrice'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PriceList />
+        <CurrentPrice />
       </div>
     )
   }
---
+++ src/components/CurrentPrice/CurrentPrice.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Box, Heading } from 'grommet'
+
+class CurrentPrice extends Component {
+  render() {
+    return (
+      <Box>
+        <Heading>Current Price</Heading>
+      </Box>
+    )
+  }
+}
+
+export default CurrentPrice
