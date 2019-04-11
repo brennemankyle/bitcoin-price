658bab3510a05359b5ceb61294c5dd38002b3505
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-04-11 14:08:34 -0600

    Add Grommet

---
+++ src/components/PriceList/PriceList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Box, Heading } from 'grommet'
+
+class PriceList extends Component {
+  render() {
+    return (
+      <Box>
+        <Heading>Prices</Heading>
+        Hello World
+      </Box>
+    )
+  }
+}
+
+export default PriceList
