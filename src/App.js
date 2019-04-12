534a852e30e900336e87416f738b2e0a3296d41e
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-04-12 11:49:15 -0600

    Add simple routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import { Grommet } from "grommet";
+import { Grommet, Anchor, Box } from "grommet";
 import { grommet } from "grommet/themes";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CurrentPriceContainer from './containers/CurrentPriceContainer/CurrentPriceContainer'
 import HistoricPriceContainer from './containers/HistoricPriceContainer/HistoricPriceContainer'
 
 class App extends Component {
+  // TODO: make fancier nav
+  // TODO: import routes and nav bar
   render() {
     return (
       <div className="App">
         <Grommet theme={grommet} full>
-          <HistoricPriceContainer />
+          <Router>
+            <div>
+              <Box>
+                <Anchor href="/">Current</Anchor>
+                <Anchor href="/historic">Historic</Anchor>
+              </Box>
+
+              <Route path="/" exact component={CurrentPriceContainer} />
+              <Route path="/historic/" component={HistoricPriceContainer} />
+            </div>
+          </Router>
         </Grommet>
       </div>
     )
